ee2bcac4774d60049bebc7f0b1186c43fc7c7788
ycombinator <ycombinator@gmail.com>
2013-06-25 21:44:56 -0700

    Using connect middleware for cookie and query param parsing.

--- server.js
+++ server.js
@@ -3,18 +3,15 @@ var http = require('http'),
     router = require(__dirname + '/lib/router.js')
     requestHandlers = require(__dirname + '/lib/request_handlers')
 
-var app = connect()
-  .use(connect.query())
-  .use(connect.cookieParser())
-
 var r = router()
 r.addRequestHandlers(requestHandlers)
 
-var server = http.createServer(function(req, res) {
-  r.route(req, res)
-}) // END - http.createServer
-
 var port = process.env.PORT || 7777
-server.listen(port, null, null, function() {
-  console.log('Streetmix router listening on port %d', port)
-}) // END - server.listen
+
+var app = connect()
+  .use(connect.cookieParser())
+  .use(connect.query())
+  .use(function(req, res) {
+    r.route(req, res)
+  })
+  .listen(port)
